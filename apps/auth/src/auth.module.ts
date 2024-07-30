8ca3fdd1f8c514ba0c9bbc168fe9951761e7d21c
Jony-Jas <jonyjasjonyjas@gmail.com>
2024-07-30 19:44:33 +0530

    passport init

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,32 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: `${configService.get('JWT_EXPIRATION')}s` },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.number().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
